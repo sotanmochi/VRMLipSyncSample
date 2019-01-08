8bc5a2ff4b829105a7a11509cf2de56842fafebc
sotanmochi <stmczk@gmail.com>
2019-01-08 19:27:54 +0900

    Add sample scenes using SALSA

---
+++ Assets/VRMLipSync/Scripts/MicrophoneInputSource.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(AudioSource))]
+public class MicrophoneInputSource : MonoBehaviour
+{
+	private AudioSource audioSource;
+
+	void Start()
+	{
+		if(Microphone.devices.Length > 0)
+		{
+			audioSource = GetComponent<AudioSource>();
+			audioSource.clip = Microphone.Start(null, true, 3, 44100);
+			audioSource.loop = true;
+
+			// Use the Attenuation of AudioMixer to mute microphone input.
+			audioSource.mute = false;
+
+			while((Microphone.GetPosition("") <= 0)){}
+			audioSource.Play();
+		}
+	}
+
+	void Update()
+	{
+		if(audioSource != null)
+		{
+			float volume = GetAveragedVolume();
+			Debug.Log("Mic input volume: " + volume);
+		}
+	}
+
+	float GetAveragedVolume()
+	{ 
+		float[] data = new float[256];
+		float a = 0;
+		audioSource.GetOutputData(data,0);
+		foreach(float s in data)
+		{
+			a += Mathf.Abs(s);
+		}
+		return a / 256.0f;
+	}
+}
